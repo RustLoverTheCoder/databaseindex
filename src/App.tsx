af6aa6c4a528567678e0a73d95a57c0ff11fd423
maxiang <maxiang@haoshuo.com>
2022-07-18 17:37:06 +0800

    add daisyUI

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,10 @@ import type {Component} from 'solid-js';
 
 const App: Component = () => {
     return (
-        <p class="text-4xl text-green-700 text-center py-20">Hello tailwind!</p>
+        <div>
+            <p class="text-4xl text-green-700 text-center py-20">Hello tailwind!</p>
+            <button class='btn'>213</button>
+        </div>
     );
 };
 
