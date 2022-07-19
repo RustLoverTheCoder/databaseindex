57dda1135a28e91f89c5c4583415eb937a89183d
maxiang <maxiang@haoshuo.com>
2022-07-19 15:07:23 +0800

    fix: ts bug

--- src/layouts/index.tsx
+++ src/layouts/index.tsx
@@ -11,9 +11,9 @@ import Support from "../components/icons/Support";
 const Layouts: Component<{ children: JSX.Element }> = ({children}) => {
     return (
         <div class='w-full h-full flex flex-col overflow-hidden'>
-            <header class='w-full h-12 flex items-center justify-between px-2 py-2 border-b'>
+            <header class='w-full h-12 flex items-center justify-between px-2 py-2'>
                 <div class='inline-flex items-center space-x-2'>
-                    <button class='btn btn-sm btn-ghost'>
+                    <button class='btn btn-sm btn-ghost hover:bg-transparent'>
                         DATABASE-INDEX
                     </button>
                 </div>
@@ -24,13 +24,20 @@ const Layouts: Component<{ children: JSX.Element }> = ({children}) => {
                         </button>
                     </div>
                     <div class="tooltip tooltip-bottom" data-tip="主题">
-                        <button class='btn btn-square btn-sm btn-ghost'>
-                            <label class='swap swap-rotate'>
-                                <input type="checkbox"/>
-                                <Moon class='w-4 h-4 swap-off'/>
-                                <Sun class='w-4 h-4 swap-on'/>
-                            </label>
-                        </button>
+                        <label class='swap swap-rotate btn btn-square btn-sm btn-ghost'>
+                            <input type="checkbox" onChange={(e: Event) => {
+                                const checked = (e.target as HTMLInputElement).checked
+                                if (!!checked) {
+                                    /// 设置夜间
+                                    document.documentElement.setAttribute("data-theme", 'dark');
+                                } else {
+                                    /// 设置日间
+                                    document.documentElement.setAttribute("data-theme", 'light');
+                                }
+                            }}/>
+                            <Moon class='w-4 h-4 swap-off'/>
+                            <Sun class='w-4 h-4 swap-on'/>
+                        </label>
                     </div>
                     <div class="tooltip tooltip-bottom" data-tip="设置">
                         <button class='btn btn-square btn-sm btn-ghost'>
@@ -45,7 +52,7 @@ const Layouts: Component<{ children: JSX.Element }> = ({children}) => {
             <div class='flex-1 flex'>
                 {children}
             </div>
-            <div class='w-full h-8 flex justify-between border-t'>
+            <div class='w-full h-8 flex justify-between'>
                 <div class='flex'>
                     <div class="tooltip tooltip-top" data-tip="云更新">
                         <button class='btn btn-sm btn-ghost hover:bg-transparent'>
